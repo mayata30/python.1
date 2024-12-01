e9741c3aeb47a4c6c3f3e1ff60b6c92869b18479
mayata30 <mayaappuz36@gmail.com>
2024-12-01 18:40:02 +0530

    Create s dictionary.py

---
+++ s dictionary.py
@@ -0,0 +1,5 @@
+my_dict={'karthika':40,'devika':2,'anjana':1,'saniya':3}
+sorted_asc=dict(sorted(my_dict.items()))
+sorted_desc=dict(sorted(my_dict.items(),reverse=True))
+print('ascending order is',sorted_asc)
+print('descending order is',sorted_desc)
